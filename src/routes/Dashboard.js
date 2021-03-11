7654813192ae9e141305720ddb2678487850ed81
ItsDavidHdez <david-0610@live.com.mx>
2021-03-11 00:25:09 -0600

    Imgs to Service Component

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
 import Experience from "../pages/Experience";
-import Services from "../pages/Services";
+import Services from "../pages/services";
 import Contact from "../pages/Contact";
 import Galery from "../pages/Galery";
 import NotFound from "../pages/NotFound";
