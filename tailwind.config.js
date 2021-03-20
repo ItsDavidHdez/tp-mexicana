585b9512591f8feb858147f9ca8c628f3a0acc72
ItsDavidHdez <david-0610@live.com.mx>
2021-03-19 20:54:36 -0600

    Colors and more config in tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+        card: "#111111",
+        border: "#171717",
+      }),
+      borderColor: (theme) => ({
+        ...theme("colors"),
+        DEFAULT: theme("colors.gray.300", "currentColor"),
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+      }),
+    },
   },
   variants: {
     extend: {},
